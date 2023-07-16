4d1b10beafa5ad8ab815d63874f821d902b42788
ccg\bedriye.koc <bedriye.koc@catconsult.com>
2023-07-16 16:06:36 -0500

    test commit

--- JenkinsWebApplicationDemo/WeatherForecast.cs
+++ JenkinsWebApplicationDemo/WeatherForecast.cs
@@ -7,7 +7,7 @@ namespace JenkinsWebApplicationDemo
         public int TemperatureC { get; set; }
 
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-
+        //test
         public string? Summary { get; set; }
     }
 }
