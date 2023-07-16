a7a44296c0671c28d19611b497907020fed78e19
ccg\bedriye.koc <bedriye.koc@catconsult.com>
2023-07-16 15:59:16 -0500

    test commit

--- JenkinsWebApplicationDemo/Controllers/WeatherForecastController.cs
+++ JenkinsWebApplicationDemo/Controllers/WeatherForecastController.cs
@@ -29,5 +29,17 @@ namespace JenkinsWebApplicationDemo.Controllers
             })
             .ToArray();
         }
+
+        [HttpGet(Name = "GetWeatherForecastALL")]
+        public IEnumerable<WeatherForecast> GetALL()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
     }
 }
